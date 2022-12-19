ed1ba42bea4476c681c74b46d59619ff11cf145b
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2022-12-20 00:55:33 +0900

    feat: 전역 스타일링(폰트) 적용
    - 전역 폰트 '고운돋움' 적용
    - 스크롤바 display 해제
    - 기본 margin, padding 0으로 초기화
    - 프로젝트 이름 및 언어 재설정

---
+++ src/static/GlobalStyles.jsx
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'GowunDodum-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/GowunDodum-Regular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;  
+    font-family: 'GowunDodum-Regular';
+  }
+
+  body {
+    box-sizing: border-box;
+  }
+
+  body {
+    scrollbar-width: none;
+    -ms-overflow-style: none;
+  }
+  
+  body::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export default GlobalStyle;

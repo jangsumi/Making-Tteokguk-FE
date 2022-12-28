dc92d28e049f8cd455247685342e6c3d049d3144
KMU\hoyong6089 <hoyong6089@kookmin.ac.kr>
2022-12-28 18:00:36 +0900

    fix : pull request 코멘트 반영
    - 닉네임 입력 창 div-> input으로 변경
    - 불필요한 fake 컴포넌트 제거
    - margin,gap,padding 등의 속성을 이용해 버튼 간 간격 조정
    - 중복된 스타일들은 재사용을 통해 코드 축소
    - 닫힌 태그 형태의 코드를 사용해 코드 간결화

--- src/pages/Init/Init.jsx
+++ src/pages/Init/Init.jsx
@@ -1,40 +1,54 @@
 import React, { useState } from "react";
 import * as styled from "./styles";
-import { Fake } from "../MySetting/styles.jsx";
 import {useNavigate} from "react-router-dom";
+import Button from "../../components/Button/Button";
 
-const Init = () => {
+// function CheckName = (NickName) =>{
+//   if (NickName.size > 6)
+// };
+// 이거는 닉네임 길이가 6자가 넘어갈 경우를 대비해서 만들었는데 아직 미완입니당..
+
+const Init = (props) => {
   const navigate = useNavigate();
-  const nickname = {name : "여섯글자별명"};
+  const RefColor = 
+    [{id : 1, RCol : "#D9D9D9"},
+    {id : 2, RCol : "#5E5E5E"},
+    {id : 3, RCol : "#FFE5A0"},
+    {id : 4, RCol : "#A5AEFF"}]
+  const RefColorList = RefColor.map((Col)=><Init key = {Col.RCol} color = {Col.RCol}/>)
+  var nickname = "";
   return (
-    <div>
       <styled.Container>
         <styled.Wrapper>
           <styled.MainText>{`친구가 나를 알아볼 수 있도록\n별명을 정해주세요`}.</styled.MainText>
           <styled.BtnContainer>
-            <styled.WhiteBtn>{nickname.name}</styled.WhiteBtn>
-            <Fake></Fake>
+            <styled.WhiteBtn>
+              <styled.NickNameInput input type="text" size="6" required="true"/>
+            </styled.WhiteBtn>
             <styled.WhosRefTxt>님의 냉장고</styled.WhosRefTxt>
           </styled.BtnContainer>
           <styled.SelectRefColTxt>냉장고 색상을 선택하세요.</styled.SelectRefColTxt>
           <styled.RefContentContainer>
             <styled.RefBgTop/>
-            <styled.RefBgBtm></styled.RefBgBtm>
+            <styled.RefBgBtm/>
             <styled.RefImgContainer>
-              <styled.WhiteRefImg></styled.WhiteRefImg>
+              <styled.WhiteRefImg/>
             </styled.RefImgContainer>
             <styled.RefContainer>
-              <styled.RefColor backgroundColor="#D9D9D9"/>
+ 
+              <styled.RefColor backgroundColor={props.RCol}/>
+              
+              {/* <styled.RefColor backgroundColor="#D9D9D9"/>
               <styled.RefColor backgroundColor="#5E5E5E"/>
               <styled.RefColor backgroundColor="#FFE5A0"/>
-              <styled.RefColor backgroundColor="#A5AEFF"/>
+              <styled.RefColor backgroundColor="#A5AEFF"/> */}
+              <styled.CheckBtnImg/>
             </styled.RefContainer>
-              <styled.CheckBtnImg></styled.CheckBtnImg>
           </styled.RefContentContainer>
           <styled.NextBtn onClick={()=>navigate('/mysetting')}>다음</styled.NextBtn>
         </styled.Wrapper>
       </styled.Container>
-    </div>  
+    
 
 
     

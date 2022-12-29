e70a1c4f4426f02184121d09fd0634cb1dd84305
Hyewon0223 <ch2w2006@kookmin.ac.kr>
2022-12-29 19:12:39 +0900

    fix: 재료 선택시 재료 수 변경 및 재료 이미지 수정

--- src/pages/SelectIngredient/Display.jsx
+++ src/pages/SelectIngredient/Display.jsx
@@ -1,36 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import * as styled from "./styles";
-import example2 from "../../images/example.svg";
+import tteok from "../../images/tteok.svg";
+import gim from "../../images/gim.svg";
+import gyeranjidan from "../../images/gyeranjidan.svg";
+import daepa from "../../images/daepa.svg";
+import yakgwa from "../../images/yakgwa.svg";
+import sanjeok from "../../images/sanjeok.svg";
+import secretIngredient from "../../images/secretIngredient.svg";
 
-const Display = ({userIngredient, onAddItem}) => {
+const Display = ({userIngredient, userSelect, setUserIngredient, onAddItem}) => {
     const ingredient = [
-        {name: "떡", imageUrl: example2},
-        {name: "김", imageUrl: example2},
-        {name: "계란지단", imageUrl: example2},
-        {name: "대파", imageUrl: example2},
-        {name: "약과", imageUrl: example2},
-        {name: "산적", imageUrl: example2},
+        {name: "떡", imageUrl: tteok},
+        {name: "김", imageUrl: gim},
+        {name: "계란지단", imageUrl: gyeranjidan},
+        {name: "대파", imageUrl: daepa},
+        {name: "약과", imageUrl: yakgwa},
+        {name: "산적", imageUrl: sanjeok},
     ]
+
+    const onClickEvent = (item) => {
+        if (userIngredient[item.index] && userSelect.length < 4) {
+            setUserIngredient(userIngredient => {
+                return [
+                    ...userIngredient.slice(0, item.index),
+                    userIngredient[item.index] - 1,
+                    ...userIngredient.slice(item.index + 1)
+                ]
+            })
+            onAddItem({ingredientIndex: item.index ,name: item.name, imageUrl: item.imageUrl});
+        }
+    }
 
     return (
         <styled.Wrapper>
             <styled.SubText>냉장고 안에 있는 재료</styled.SubText>
             <styled.Ingredients>
                 {ingredient.map(({name, imageUrl}, index) =>
-                    <styled.IngredientWrapper key={`ingredient-${index}`} onClick={() => onAddItem({name, imageUrl})}>
-                        {userIngredient[index] ?
+                    <styled.IngredientWrapper key={`ingredient-${index}`}
+                                              active={userIngredient[index]}
+                                              onClick={()=>onClickEvent({index, name, imageUrl})}>
                             <styled.IngredientBox imageUrl={imageUrl}>
                                 <styled.IngredientNumber>x {userIngredient[index]}</styled.IngredientNumber>
+                                {!userIngredient[index] && <styled.IngredientLock/>}
                             </styled.IngredientBox>
-                            :
-                            <styled.IngredientLock/>
-                        }
                         <styled.IngredientText>{name}</styled.IngredientText>
                     </styled.IngredientWrapper>
                 )}
-                <styled.SpecialBox>
+                <styled.SpecialBox active={userIngredient[6]}
+                                   onClick={()=>onClickEvent({index:6, name:"비밀의 재료", imageUrl:secretIngredient})}>
                     <styled.SpecialText>비밀의 재료</styled.SpecialText>
                     <styled.IngredientNumber>x {userIngredient[6] || 0}</styled.IngredientNumber>
+                    {!userIngredient[6] && <styled.SecretLock/>}
                 </styled.SpecialBox>
             </styled.Ingredients>
         </styled.Wrapper>

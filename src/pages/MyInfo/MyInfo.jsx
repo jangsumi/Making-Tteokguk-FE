900de4df3beb18c809a997be455a9676c2b320e9
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-01-01 13:56:45 +0900

    refactor: '내가 만든 떡국' 화면 코드 리팩토링
    - map 함수를 사용하여 코드 간략화
    - 사용자별 떡국 해금 상황을 props로 전달 받는 형식으로 코드 수정

--- src/pages/MyInfo/MyInfo.jsx
+++ src/pages/MyInfo/MyInfo.jsx
@@ -10,31 +10,37 @@ import tgOnlytteok from "../../images/tgOnlytteok.svg";
 import tgOnlyegg from "../../images/tgOnlyegg.svg";
 import tgBlack from "../../images/tgBlack.svg";
 
-const Cooked = () => {
-  const unlock = [false, false, false, false, true];
+const Cooked = ({unlock}) => {
+  const infoTteokguk = [
+    { index: 0, name: "이상적인 떡국", imgSrc: tgIdeal },
+    { index: 1, name: "취향이 보이는 떡국", imgSrc: tgMyTaste },
+    { index: 2, name: "계란국을 빙자한 떡국", imgSrc: tgOnlyegg },
+    { index: 3, name: "먹물 떡국", imgSrc: tgBlack },
+    { index: 4, name: "떡국이 떡만 있어서 떡국", imgSrc: tgOnlytteok },
+    { index: 5, name: "매운 떡국", imgSrc: tgSpicy },
+    { index: 6, name: "이상한 떡국", imgSrc: tgStrange },
+  ];
 
   return (
     <styled.FlexBox>
       <styled.GridContainer>
-        <styled.FlexBox direction="column" gap="4px" aCenter={true}>
-          <styled.Tteokguk background={tgIdeal}>
-            {!false && <styled.TteockgukLock />}
-          </styled.Tteokguk>
-          <styled.FontDiv fontSize="12px">
-            {false ? "취향이 보이는 떡국" : "???"}
-          </styled.FontDiv>
-        </styled.FlexBox>
-
-        <styled.Tteokguk />
-        <styled.Tteokguk />
-        <styled.Tteokguk />
-        <styled.Tteokguk />
+        {infoTteokguk.map((item) => (
+          <styled.FlexBox direction="column" gap="4px" aCenter={true}>
+            <styled.Tteokguk background={item.imgSrc}>
+              {!unlock[item.index] && <styled.TteockgukLock />}
+            </styled.Tteokguk>
+            <styled.FontDiv fontSize="12px">
+              {unlock[item.index] ? item.name : "???"}
+            </styled.FontDiv>
+          </styled.FlexBox>
+        ))}
       </styled.GridContainer>
     </styled.FlexBox>
   );
 };
 
 const MyInfo = () => {
+  const unlock = [false, true, true, false, true];
   const [currPage, setCurrPage] = useState(0);
   const userMessage = [
     {
@@ -104,7 +110,7 @@ const MyInfo = () => {
           받은 덕담
         </styled.NaviItem>
       </styled.FlexBox>
-      {currPage === 0 && <Cooked />}
+      {currPage === 0 && <Cooked unlock={unlock} />}
       {currPage === 1 && <Message message={userMessage} />}
     </styled.FlexBox>
   );

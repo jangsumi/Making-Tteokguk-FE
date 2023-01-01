358244590d72d7c851f0585e963188dda9417327
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-01-01 16:40:44 +0900

    feat: 재료 선물 메세지 모달 수정
    - 회원 / 비회원을 분기하여 모달을 2가지로 변경
    - 모달(회원)의 경우, 기존 depth를 2에서 1로 감소

--- src/components/Modal/MessageModal.jsx
+++ src/components/Modal/MessageModal.jsx
@@ -1,26 +1,20 @@
-import React, {useState, useEffect} from 'react';
-import Modal from "./Modal.jsx"
-import * as styled from './styles.jsx';
+import React from "react";
+import Modal from "./Modal.jsx";
+import * as styled from "./styles.jsx";
 
-const MessageModal = ({onConfirmClick, secret}) => {
-    const [message, setMessage] = useState('덕담과 함께 떡국 재료를\n친구의 냉장고에 넣어뒀어요!');
-    const [clickEvent, setClickEvent] = useState(()=>onConfirmClick);
-
-    const onNextClick = () => {
-        setMessage('‘비밀의 재료’를 얻었어요!\n나의 냉장고에서 확인해보세요.');
-        setClickEvent(()=>onConfirmClick);
-    }
-
-    useEffect(()=>{
-        if (secret) setClickEvent(()=>onNextClick);
-    },[]);
-
-    return (
-        <Modal>
-            <styled.Text fontSize={16}>{message}</styled.Text>
-            <styled.Button onClick={clickEvent} width={122}>확인</styled.Button>
-        </Modal>
-    );
+const MessageModal = ({ onConfirmClick, secret }) => {
+  return (
+    <Modal>
+      <styled.Text fontSize={16}>
+        {secret
+          ? "덕담과 재료를 냉장고에 잘 넣어뒀어요!\n'비밀의 재료'를 얻었으니\n나의 냉장고에서 확인해 보세요."
+          : "덕담과 함께 떡국 재료를\n 친구의 냉장고에 넣어뒀어요!"}
+      </styled.Text>
+      <styled.Button onClick={onConfirmClick} width={122}>
+        확인
+      </styled.Button>
+    </Modal>
+  );
 };
 
 export default MessageModal;

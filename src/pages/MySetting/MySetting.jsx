4adacd5505630cd50f1837efcb1b67ea01d41a38
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-01-03 22:48:41 +0900

    refactor: MySetting 페이지 코드 구조 변경
    - 체크박스 버튼 이미지가 출력되지 않던 현상 수정
    - 체크박스 버튼 active 로직 수정 -> useEffect를 사용하여 check2, 3에 값을 관측하여 check1의 값이 변경되도록 구현
    - 체크박스 버튼 클릭 이벤트 삭제 이후 onClick 함수 body에서 구현
    - Container의 경우, props를 전달하여 재사용할 수 있어 수정 (TextDiv, FlexBox를 새로 정의 후 사용)

--- src/pages/MySetting/MySetting.jsx
+++ src/pages/MySetting/MySetting.jsx
@@ -1,157 +1,131 @@
 import React, { useEffect, useState } from "react";
 import * as styled from "./styles";
-import {useNavigate} from "react-router-dom";
-import checked from '../../images/Checked.svg';
-import unChecked from '../../images/UnChecked.svg';
-
-
+import { useNavigate } from "react-router-dom";
 
 const MySetting = () => {
   const navigate = useNavigate();
-  const [pubCheck, setPubCheck] = useState("false");
-  const [pubSel, setPubsel] = useState("false");
+  const [pubCheck, setPubCheck] = useState(false);
+  const [pubSel, setPubsel] = useState(false);
   const [color, setColor] = useState("#CFCFCF");
   const [color2, setColor2] = useState("#CFCFCF");
   const pubBtn = (e) => {
-    setPubCheck(true);
     setColor("#E0AEBC");
     setColor2("#CFCFCF");
     setPubsel(true);
-  
+    setPubCheck(true);
   };
 
   const prvBtn = (e) => {
-    setPubCheck(false);
     setColor("#CFCFCF");
     setColor2("#E0AEBC");
-    setPubsel(true);
+    setPubsel(false);
+    setPubCheck(true);
   };
 
-  const checkActive = (pubSel) => {
-    if (pubSel === true && check1 === true || pubSel === true && (check2 === true && check3 === true)){
-      return true;
-    }
-    else{
-      return false;
-    }
-  }
-  //Y는 체크된 경우 , N은 체크 안된경우
-  const [check1, setCheck1] = useState(true);
-  const [check2, setCheck2] = useState(true);
-  const [check3, setCheck3] = useState(true);
-  const [allCheck, setAllCheck] = useState(false);
-  const checkBox1 = () =>{
-    if (check1 === true){
-      setCheck1(false);
-      setCheck2(false);
-      setCheck3(false);
-      setAllCheck(false);
-      console.log("1번 체크 : ",check1);
-    } else if (check1 === false){
+  const [check1, setCheck1] = useState(false);
+  const [check2, setCheck2] = useState(false);
+  const [check3, setCheck3] = useState(false);
+
+  useEffect(() => {
+    if (check2 && check3) {
       setCheck1(true);
-      setCheck2(true);
-      setCheck3(true);
-      setAllCheck(true);
-      console.log("1번 체크 : ",check1);
+    } else {
+      setCheck1(false);
     }
-
-  };
-
-  const checkBox2 = () => {
-    if (check2 === true){
-      setCheck2(false);
-      if (setAllCheck() === true)
-      {
-        setAllCheck(false);
-        setCheck1(false);
-        console.log("체크해제");
-      }
-      console.log("2번 체크 : ",check2);
-    } else if (check2 === false){
-      setCheck2(true);
-      console.log("2번 체크 : ",check2);
-    }
-  };
-
-  const checkBox3 = () =>{
-    if (check3 === true){
-      setCheck3(false);
-      console.log("3번 체크 : ",check3);
-
-    } else if (check3 === false){
-      setCheck3(true);
-      console.log("3번 체크 : ",check3);
-    }
-  };
-
+  }, [check2, check3]);
 
   return (
-      <styled.Container>
-        <styled.Wrapper>
-          <styled.MainText>덕담 개수 공개 여부</styled.MainText>
-          <styled.MsgPubTxt>{`다른 사람에게\n받은 덕담의 개수를 공개할까요?`}</styled.MsgPubTxt>
-          <styled.ExMarkContainer>
-            <styled.ExMarkImg/>
-            <styled.MsgPubInfo>공개할 때만 남이 내 냉장고를 열어볼 수 있어요.</styled.MsgPubInfo>
-          </styled.ExMarkContainer>
-          <styled.BtnContainer>
-            <styled.PubBtn
-              color = {color} 
-              onClick={pubBtn}
-              >공개 할래요!
-              </styled.PubBtn>
-            <styled.PrvBtn 
-              color = {color2} 
-              onClick={prvBtn}
-              >나만 볼래요.</styled.PrvBtn>
-          </styled.BtnContainer>
-          <styled.CenterLine/>
-          <styled.AgreeTxt>약관 동의</styled.AgreeTxt>
-          <styled.GroupContainer>
-            <styled.CheckBtn1Img
-            ischecked1 = {check1}
-            onClick = {()=>checkBox1()}
-            value = {check1}
-            url = {check1 === true ? {checked} : {unChecked}}
-            />
-            <styled.AgreeAll>전체 동의</styled.AgreeAll>
-          </styled.GroupContainer>
-          <styled.GroupContainer>
-            <styled.CheckBtn2Img
-            ischecked2 = {check2}
-            onClick = {()=>checkBox2()}
-            url = {check2 === true ? {checked} : {unChecked}}
-            />
-            <styled.AgeCheck>(필수) 만 14세 이상이예요.</styled.AgeCheck>
-          </styled.GroupContainer>
-          <styled.GroupContainer>
-            <styled.CheckBtn3Img
-            ischecked3 = {check3}
-            onClick = {()=>checkBox3()}
-            url = {check3 === true ? {checked} : {unChecked}}
-            />
-            <styled.Agree>(필수) 이용약관 및 개인정보수집이용에 동의해요.</styled.Agree>
-          </styled.GroupContainer>
-          <styled.BtnContainer>
-            <styled.PrevBtn
-              active={true}
-              onClick={() => {
-                navigate('/init');
-              }}
-            >
-              {"이전"}
-            </styled.PrevBtn>
-            <styled.NextBtn
-              active={checkActive(pubSel)}
-              onClick={() => {
-                checkActive(pubSel) && navigate('/refrigerator');
-              }}
-            >
-              {"시작하기"}
-            </styled.NextBtn>
-          </styled.BtnContainer>
-        </styled.Wrapper>
-      </styled.Container>
+    <styled.FlexBox direction={"column"} alignItems={"center"}>
+      <styled.FlexBox direction={"column"}>
+        <styled.FlexBox direction={"column"}>
+          <styled.TextDiv fontSize={"20px"} margin={"40px 0px 0px 0px"}>
+            덕담 개수 공개 여부
+          </styled.TextDiv>
+          <styled.TextDiv fontSize={"16px"} margin={"11px 0px 6px 0px"}>
+            다른 사람에게
+            <br />
+            받은 덕담의 개수를 공개할까요?
+          </styled.TextDiv>
+        </styled.FlexBox>
+        <styled.FlexBox alignItems={"center"} gap={"6px"}>
+          <styled.ExMarkImg />
+          <styled.TextDiv fontSize={"12px"}>
+            공개할 때만 남이 내 냉장고를 열어볼 수 있어요.
+          </styled.TextDiv>
+        </styled.FlexBox>
+        <styled.FlexBox
+          alignItems={"center"}
+          gap={"19px"}
+          margin={"6px 0px 0px 0px"}
+        >
+          <styled.PubBtn color={color} onClick={pubBtn}>
+            공개 할래요!
+          </styled.PubBtn>
+          <styled.PrvBtn color={color2} onClick={prvBtn}>
+            나만 볼래요.
+          </styled.PrvBtn>
+        </styled.FlexBox>
+        <styled.CenterLine />
+        <styled.TextDiv fontSize={"20px"} margin={"40px 0px 10px 0px"}>
+          약관 동의
+        </styled.TextDiv>
+        <styled.FlexBox alignItems={"center"} gap={"10px"}>
+          <styled.CheckBtn
+            onClick={() => {
+              if (check1) {
+                setCheck2(false);
+                setCheck3(false);
+              } else {
+                setCheck2(true);
+                setCheck3(true);
+              }
+            }}
+            isChecked={check1}
+          />
+          <styled.TextDiv fontSize={"14px"}>전체 동의</styled.TextDiv>
+        </styled.FlexBox>
+        <styled.FlexBox
+          alignItems={"center"}
+          gap={"10px"}
+          margin={"20px 0px 6px 0px"}
+        >
+          <styled.CheckBtn
+            onClick={() => setCheck2(!check2)}
+            isChecked={check2}
+          />
+          <styled.TextDiv fontSize={"14px"}>
+            (필수) 만 14세 이상이에요.
+          </styled.TextDiv>
+        </styled.FlexBox>
+        <styled.FlexBox alignItems={"center"} gap={"10px"}>
+          <styled.CheckBtn
+            onClick={() => setCheck3(!check3)}
+            isChecked={check3}
+          />
+          <styled.TextDiv fontSize={"14px"}>
+            (필수) 이용약관 및 개인정보수집이용에 동의해요.
+          </styled.TextDiv>
+        </styled.FlexBox>
+        <styled.FlexBox gap={"10px"}>
+          <styled.PrevBtn
+            active={true}
+            onClick={() => {
+              navigate("/init");
+            }}
+          >
+            이전
+          </styled.PrevBtn>
+          <styled.NextBtn
+            active={check1 && pubCheck}
+            onClick={() => {
+              check1 && pubCheck && navigate("/refrigerator");
+            }}
+          >
+            시작하기
+          </styled.NextBtn>
+        </styled.FlexBox>
+      </styled.FlexBox>
+    </styled.FlexBox>
   );
 };
 

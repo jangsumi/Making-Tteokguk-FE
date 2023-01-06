a42ff72746f5c8d60d3e8da3923cb7c537bc65f3
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-01-07 00:48:16 +0900

    feat: 계정 연결 도중 에러 발생 시 메인 이동 처리

--- src/pages/Main/KakaoAuth.jsx
+++ src/pages/Main/KakaoAuth.jsx
@@ -1,22 +1,29 @@
 import axios from "axios";
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import SyncLoader from "react-spinners/SyncLoader.js";
 import { Container } from "./styles";
 
 const KakaoAuth = () => {
   // 인가 코드
   let code = new URL(window.location.href).searchParams.get("code");
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios({
       method: "GET",
       url: `http://백엔드/user/kakao/callback?code=${code}`,
-    }).then((res) => {
-      console.log(res); // 토큰 값 확인용 코드
-      const ACCESS_TOKEN = res.data.acessToken;
-      console.log(ACCESS_TOKEN);
-    });
-  }, [code]);
+    })
+      .then((res) => {
+        console.log(res); // 토큰 값 확인용 코드
+        const ACCESS_TOKEN = res.data.acessToken;
+        console.log(ACCESS_TOKEN);
+      })
+      .catch((err) => {
+        window.alert(err, "로그인 실패");
+        navigate("/");
+      });
+  }, []);
   return (
     <Container>
       <SyncLoader color="#DD7592" margin={4} size={12} />

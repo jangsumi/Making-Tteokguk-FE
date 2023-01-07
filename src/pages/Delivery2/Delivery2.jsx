cc6a0db1b2afa035818db83a386133d9c2198833
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-01-07 10:09:56 +0900

    fix: TopBar 중앙정렬 미적용 해결
    - TopBar 좌측 뒤로가기 버튼 크기 조정
    - Delivery, Delivery2 페이지 구조 변경

--- src/pages/Delivery2/Delivery2.jsx
+++ src/pages/Delivery2/Delivery2.jsx
@@ -14,9 +14,9 @@ const Delivery2 = () => {
   const location = useLocation();
   const { ingredient } = location.state || false;
 
-  useEffect(()=> {
-    if (!ingredient) navigate('/delivery');
-  },[])
+  useEffect(() => {
+    if (!ingredient) navigate("/delivery");
+  }, []);
 
   const onChangeName = (e) => {
     setUserName(e.target.value.slice(0, 6));
@@ -35,58 +35,63 @@ const Delivery2 = () => {
 
   const onButtonClick = () => {
     setShowModal(true);
-    console.log({ingredient: ingredient, name: userName, message: textMessage});
-  }
+    console.log({
+      ingredient: ingredient,
+      name: userName,
+      message: textMessage,
+    });
+  };
 
   return (
     <styled.Container>
       <TopBar titleName={"떡국 재료 선물하기"} />
-
-      <div style={{marginTop: 30}}>
-        <styled.FontDiv fontSize="20px">별명을 정해주세요.</styled.FontDiv>
-        <styled.NameInput
-          type="text"
-          name="username"
-          placeholder="별명을 입력해주세요. (6글자 이내)"
-          spellcheck="false"
-          value={userName}
-          onChange={onChangeName}
-        />
-      </div>
-
-      <div>
-        <styled.FontDiv fontSize="20px">덕담을 작성해주세요.</styled.FontDiv>
-        <styled.TextContainer>
-          <styled.MsgTxtArea
-            name="textMessage"
-            placeholder="친구에게 새해 덕담을 남겨 보세요. (50자 이내)"
+      <styled.Container>
+        <div style={{ marginTop: 30 }}>
+          <styled.FontDiv fontSize="20px">별명을 정해주세요.</styled.FontDiv>
+          <styled.NameInput
+            type="text"
+            name="username"
+            placeholder="별명을 입력해주세요. (6글자 이내)"
             spellcheck="false"
-            contenteditable="true"
-            rows={4}
-            value={textMessage}
-            onChange={onChangeMessage}
+            value={userName}
+            onChange={onChangeName}
           />
-          <styled.TextCounter>({textMessage.length}/50)</styled.TextCounter>
-        </styled.TextContainer>
-        <styled.FlexDiv gap="6px" margin="0 0 36px">
-          <img src={bang} />
-          <styled.FontDiv fontSize="12px">
-            상대를 비방하는 내용 작성 시 법적 처벌을 받을 수 있습니다.
-          </styled.FontDiv>
-        </styled.FlexDiv>
-      </div>
+        </div>
 
-      <Button
-        onClickEvent={onButtonClick}
-        text={"떡국 재료 선물하기"}
-        active={userName.trim() != "" && textMessage.trim() != ""}
-      />
-      {showModal && (
-        <MessageModal
-          onConfirmClick={() => confirmEvent("message")}
-          secret={true}
+        <div>
+          <styled.FontDiv fontSize="20px">덕담을 작성해주세요.</styled.FontDiv>
+          <styled.TextContainer>
+            <styled.MsgTxtArea
+              name="textMessage"
+              placeholder="친구에게 새해 덕담을 남겨 보세요. (50자 이내)"
+              spellcheck="false"
+              contenteditable="true"
+              rows={4}
+              value={textMessage}
+              onChange={onChangeMessage}
+            />
+            <styled.TextCounter>({textMessage.length}/50)</styled.TextCounter>
+          </styled.TextContainer>
+          <styled.FlexDiv gap="6px" margin="0 0 36px">
+            <img src={bang} />
+            <styled.FontDiv fontSize="12px">
+              상대를 비방하는 내용 작성 시 법적 처벌을 받을 수 있습니다.
+            </styled.FontDiv>
+          </styled.FlexDiv>
+        </div>
+
+        <Button
+          onClickEvent={onButtonClick}
+          text={"떡국 재료 선물하기"}
+          active={userName.trim() != "" && textMessage.trim() != ""}
         />
-      )}
+        {showModal && (
+          <MessageModal
+            onConfirmClick={() => confirmEvent("message")}
+            secret={true}
+          />
+        )}
+      </styled.Container>
     </styled.Container>
   );
 };

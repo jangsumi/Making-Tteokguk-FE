381ce9975810ff500e91a390863cbf55a7a1f871
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-01-07 09:19:14 +0900

    feat: 로그인 에러 모달 구현
    - 기존 alert 창을 모달로 대체

--- src/pages/Main/KakaoAuth.jsx
+++ src/pages/Main/KakaoAuth.jsx
@@ -1,33 +1,47 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import SyncLoader from "react-spinners/SyncLoader.js";
+import LoginErrorModal from "../../components/Modal/LoginErrorModal";
 import { Container } from "./styles";
 
 const KakaoAuth = () => {
-  // 인가 코드
+  const [showModal, setShowModal] = useState(false);
+
+  const confirmEvent = () => {
+    setShowModal(false);
+    navigate("/");
+  };
+
   let code = new URL(window.location.href).searchParams.get("code");
   const navigate = useNavigate();
 
   useEffect(() => {
     axios({
       method: "GET",
-      url: `http://백엔드/user/kakao/callback?code=${code}`,
+      url: `http://localhost:5173//user/kakao/callback?code=${code}`,
     })
       .then((res) => {
         console.log(res); // 토큰 값 확인용 코드
         const ACCESS_TOKEN = res.data.acessToken;
         console.log(ACCESS_TOKEN);
+        if (res.status === 203) {
+          // 가입되지 않은 사용자의 경우
+          navigate("/init");
+        } else if (res.status === 200) {
+          navigate("/refrigerator");
+        }
       })
       .catch((err) => {
-        window.alert(err, "로그인 실패");
-        navigate("/");
+        console.log(err);
+        setTimeout(setShowModal(true), 500);
       });
   }, []);
   return (
     <Container>
       <SyncLoader color="#DD7592" margin={4} size={12} />
       <>카카오 계정을 연결하고 있어요.</>
+      {showModal && <LoginErrorModal onConfirmClick={() => confirmEvent()} />}
     </Container>
   );
 };

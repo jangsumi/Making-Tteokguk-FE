51a858877f3382b4c13f51ee3af913d91daa1b97
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2023-01-11 22:49:07 +0900

    feat: kakaoTest

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import * as styled from "./styles";
 
 const MainPage = () => {
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_APP_REST_API_KEY}&redirect_uri=${import.meta.env.VITE_APP_REDIRECT_URI}&response_type=code`;
+
+    
+    const REDIRECT_URI =  "http://3.35.136.13:8080/api/user/kakao/callback";
+
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    console.log(KAKAO_AUTH_URL);
+
     const handleLogin = () => {
         window.location.href = KAKAO_AUTH_URL;
     };

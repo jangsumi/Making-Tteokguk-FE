7c042cc78714a8906bf4bfccd6b651190007de5e
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-01-14 01:57:58 +0900

    feat: Init 페이지 useEffect 추가
    - 비정상적인 방식의 접근 차단 (kakaoId state를 받지 못한 경우, 메인 페이지로 이동)

--- src/pages/Init/Init.jsx
+++ src/pages/Init/Init.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import * as styled from "./styles";
 import Button from "../../components/Button/Button";
@@ -8,68 +8,82 @@ import BlackRef from "../../images/BlackRef.svg";
 import PurpleRef from "../../images/PurpleRef.svg";
 
 const Init = () => {
-  const RefColor =
-    [
-      {id : 0, rColor : "#D9D9D9"},
-      {id : 1, rColor : "#5E5E5E"},
-      {id : 2, rColor : "#FFE5A0"},
-      {id : 3, rColor : "#A5AEFF"},
-    ]
+  const RefColor = [
+    { id: 0, rColor: "#D9D9D9" },
+    { id: 1, rColor: "#5E5E5E" },
+    { id: 2, rColor: "#FFE5A0" },
+    { id: 3, rColor: "#A5AEFF" },
+  ];
 
   const navigate = useNavigate();
   const location = useLocation();
-  const [nickName, setNickName] = useState(location.state?.nickName || '');
+  const [nickName, setNickName] = useState(location.state?.nickName || "");
   const [RefCol, setRefCol] = useState(location.state?.RefCol || 0);
+  const { kakaoId } = location.state || false;
+  const onChange = (e) => setNickName(e.target.value.trimStart().slice(0, 6));
+  const onBlur = (e) => setNickName(nickName.trimEnd().slice(0, 6));
 
-  const onChange = (e) => setNickName(e.target.value.trimStart().slice(0,6));
-  const onBlur = (e) => setNickName(nickName.trimEnd().slice(0,6))
+  useEffect(() => {
+    console.log(kakaoId);
+    if (!kakaoId) navigate("/");
+  }, []);
 
   const onButtonClick = () => {
-    navigate('/mysetting', {state:{nickName, RefCol}});
-    console.log({ nickName, RefCol })
-  }
+    navigate("/mysetting", { state: { nickName, RefCol } });
+    console.log({ nickName, RefCol });
+  };
 
   return (
-      <styled.Container>
-          <styled.MainText>{`친구가 나를 알아볼 수 있도록\n별명을 정해주세요`}.</styled.MainText>
-          <styled.BtnContainer>
-            <styled.WhiteBtn>
-              <styled.NickNameInput
-                input type="text"
-                maxLength='6'
-                onChange={onChange}
-                onBlur={onBlur}
-                value = {nickName || ''}/>
-            </styled.WhiteBtn>
-            <styled.WhosRefTxt>님의 냉장고</styled.WhosRefTxt>
-          </styled.BtnContainer>
-          <styled.SelectRefColTxt>냉장고 색상을 선택하세요.</styled.SelectRefColTxt>
-          <styled.RefBgTop>
-            <styled.RefBgBtm>
-              <styled.RefContainer>
-                {RefColor.map((item)=> {
-                  return <styled.RefColor
-                    key={`refColor-${item.id}`}
-                    backgroundColor={item.rColor}
-                    onClick = {() => setRefCol(item.id)}
-                  >
-                    {item.id === RefCol && <styled.CheckBtnImg/>}
-                  </styled.RefColor>
-                })}
-              </styled.RefContainer>
-            </styled.RefBgBtm >
-            {[WhiteRef, BlackRef, YellowRef, PurpleRef].map((ref, index)=>{
-              return <styled.RefImg key={`ref-${index}`} imageUrl={ref}
-                                    refVisible={index === RefCol} />
-            })}
-
-          </styled.RefBgTop>
-          <Button
-            active={nickName !== ""}
-            text = "다음"
-            onClickEvent={onButtonClick}
+    <styled.Container>
+      <styled.MainText>
+        {`친구가 나를 알아볼 수 있도록\n별명을 정해주세요`}.
+      </styled.MainText>
+      <styled.BtnContainer>
+        <styled.WhiteBtn>
+          <styled.NickNameInput
+            input
+            type="text"
+            maxLength="6"
+            onChange={onChange}
+            onBlur={onBlur}
+            value={nickName || ""}
           />
-      </styled.Container>
+        </styled.WhiteBtn>
+        <styled.WhosRefTxt>님의 냉장고</styled.WhosRefTxt>
+      </styled.BtnContainer>
+      <styled.SelectRefColTxt>냉장고 색상을 선택하세요.</styled.SelectRefColTxt>
+      <styled.RefBgTop>
+        <styled.RefBgBtm>
+          <styled.RefContainer>
+            {RefColor.map((item) => {
+              return (
+                <styled.RefColor
+                  key={`refColor-${item.id}`}
+                  backgroundColor={item.rColor}
+                  onClick={() => setRefCol(item.id)}
+                >
+                  {item.id === RefCol && <styled.CheckBtnImg />}
+                </styled.RefColor>
+              );
+            })}
+          </styled.RefContainer>
+        </styled.RefBgBtm>
+        {[WhiteRef, BlackRef, YellowRef, PurpleRef].map((ref, index) => {
+          return (
+            <styled.RefImg
+              key={`ref-${index}`}
+              imageUrl={ref}
+              refVisible={index === RefCol}
+            />
+          );
+        })}
+      </styled.RefBgTop>
+      <Button
+        active={nickName !== ""}
+        text="다음"
+        onClickEvent={onButtonClick}
+      />
+    </styled.Container>
   );
 };
 
